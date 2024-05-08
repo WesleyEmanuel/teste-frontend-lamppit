4d8682394602d990e6cf6a3a24536a1728dad273
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2024-05-07 22:03:28 -0300

    faet: ordenacao por titulo e data de vencimento em minhas rendas fixas

--- teste-angular/src/app/renda-fixa/components/minhas-rendas-fixas-table/minhas-rendas-fixas-table.component.ts
+++ teste-angular/src/app/renda-fixa/components/minhas-rendas-fixas-table/minhas-rendas-fixas-table.component.ts
@@ -4,25 +4,68 @@ import { RendasFixas } from '../../models/RendasFixas';
 @Component({
   selector: 'app-minhas-rendas-fixas-table',
   templateUrl: './minhas-rendas-fixas-table.component.html',
-  styleUrl: './minhas-rendas-fixas-table.component.scss'
+  styleUrl: './minhas-rendas-fixas-table.component.scss',
 })
-export class MinhasRendasFixasTableComponent implements OnInit{
+export class MinhasRendasFixasTableComponent implements OnInit {
   @Input() rendasFixas!: RendasFixas;
 
   search: string = '';
 
-  constructor() { }
+  listOrder: '1' | '2' | '3' = '1';
+
+  filterAndOrderRendasFixas: any;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.filterAndOrderRendasFixas = this.rendasFixas.snapshotByProduct;
   }
 
-  filteredRendasFixas(filter: string): RendasFixas[] {
-    if(filter){
-      return this.rendasFixas.snapshotByProduct.filter((rendaFixa: any) => {
-        return rendaFixa.fixedIncome.name.toLowerCase().includes(filter.toLowerCase());
-      })
+  filteredRendasFixas() {
+    if (this.search) {
+      this.filterAndOrderRendasFixas =
+        this.rendasFixas.snapshotByProduct.filter((rendaFixa: any) => {
+          return rendaFixa.fixedIncome.name
+            .toLowerCase()
+            .includes(this.search.toLowerCase());
+        });
+    } else {
+      this.filterAndOrderRendasFixas = this.rendasFixas.snapshotByProduct;
     }
 
-    return this.rendasFixas.snapshotByProduct;
+    switch (this.listOrder) {
+      case '2':
+        this.filterAndOrderRendasFixas.sort((a: any, b: any) => {
+          if (a.fixedIncome.name < b.fixedIncome.name) {
+            return -1;
+          }
+          if (a.fixedIncome.name > b.fixedIncome.name) {
+            return 1;
+          }
+          return 0;
+        });
+
+        break;
+
+      case '3':
+        this.filterAndOrderRendasFixas.sort((a: any, b: any) => {
+          let dateA = a.due.date.split('/').reverse().join('-');
+
+          let dateB = b.due.date.split('/').reverse().join('-');
+
+          if (dateA < dateB) {
+            return -1;
+          }
+          if (dateA > dateB) {
+            return 1;
+          }
+          return 0;
+        });
+
+        break;
+
+      default:
+        break;
+    }
   }
 }
